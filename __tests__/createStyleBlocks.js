e0d8be289fbdf580f86b791460a7e0f940af6a74
sainthkh <sainthkh@naver.com>
2017-02-27 17:37:32 +0900

    Added createStyleBlocks function.

--- __tests__/createStyleBlocks.js
+++ __tests__/createStyleBlocks.js
@@ -1,5 +1,120 @@
-const { selectorPath } = require('../css2scss')
+const postcss = require('postcss')
+const { createStyleBlocks } = require('../css2scss')
 
-test("null", () => {
-	
+test("correct path", () => {
+	const style = ".home { display: block; }"
+	const root = postcss.parse(style)
+	var blocks = createStyleBlocks(root)
+	expect(blocks[0].path).toEqual([".home"])
+	expect(blocks[0].nodes).toEqual(root.nodes[0].nodes)
+})
+
+test("correct complicated path", () => {
+	const style = ".home .action { display: flex; flex-direction: column; }"
+	const root = postcss.parse(style)
+	var blocks = createStyleBlocks(root)
+	expect(blocks[0].path).toEqual([".home", ".action"])
+	expect(blocks[0].nodes).toEqual(root.nodes[0].nodes)
+})
+
+test("multiple style blocks", () => {
+	const style = `
+.home {
+	display: block;
+}
+
+.home .action {
+	display: flex;
+	flex-direction: column;
+}
+`
+	const root = postcss.parse(style)
+	var blocks = createStyleBlocks(root)
+	expect(blocks[0].path).toEqual([".home"])
+	expect(blocks[0].nodes).toEqual(root.nodes[0].nodes)
+	expect(blocks[1].path).toEqual([".home", ".action"])
+	expect(blocks[1].nodes).toEqual(root.nodes[1].nodes)
+})
+
+test("media query block", () => {
+	const style = `
+.home {
+	display: block;
+}
+
+.home .action {
+	display: flex;
+	flex-direction: row;
+}
+
+@media (min-width: 600px) {
+	.home .action {
+		flex-direction: column;
+	}
+}
+`
+	const root = postcss.parse(style)
+	var blocks = createStyleBlocks(root)
+	expect(blocks[2].path).toEqual([".home", ".action", "@media (min-width: 600px)"])
+	expect(blocks[2].nodes).toEqual(root.nodes[2].nodes[0].nodes)
+})
+
+test("multiple media query blocks", () => {
+	const style = `
+.home {
+	display: block;
+}
+
+.home .action {
+	display: flex;
+	flex-direction: row;
+}
+
+@media (min-width: 600px) {
+	.home .action {
+		flex-direction: column;
+	}
+}
+
+@media (min-width: 960px) {
+	.home .action {
+		flex-wrap: true;
+	}
+}
+`
+	const root = postcss.parse(style)
+	var blocks = createStyleBlocks(root)
+	expect(blocks[2].path).toEqual([".home", ".action", "@media (min-width: 600px)"])
+	expect(blocks[2].nodes).toEqual(root.nodes[2].nodes[0].nodes)
+	expect(blocks[3].path).toEqual([".home", ".action", "@media (min-width: 960px)"])
+	expect(blocks[3].nodes).toEqual(root.nodes[3].nodes[0].nodes)
+})
+
+test("multiple blocks in media query block", () => {
+	const style = `
+.home {
+	display: block;
+}
+
+.home .action {
+	display: flex;
+	flex-direction: row;
+}
+
+@media (min-width: 600px) {
+	.home {
+		display: table;
+	}
+
+	.home .action {
+		flex-direction: column;
+	}
+}
+`
+	const root = postcss.parse(style)
+	var blocks = createStyleBlocks(root)
+	expect(blocks[2].path).toEqual([".home", "@media (min-width: 600px)"])
+	expect(blocks[2].nodes).toEqual(root.nodes[2].nodes[0].nodes)
+	expect(blocks[3].path).toEqual([".home", ".action", "@media (min-width: 600px)"])
+	expect(blocks[3].nodes).toEqual(root.nodes[2].nodes[1].nodes)
 })
--- css2scss.js
+++ css2scss.js
@@ -1,6 +1,6 @@
 const postcss = require('postcss')
 
-exports.selectorPath = (selector, media) => {
+const selectorPath = (selector, media) => {
 	selector = selector.replace(/\s*(:|~|\+|>)\s*/g, '$1')
 	selector = selector.replace(/(:|~|\+|>)/g, ' &$1')
 	selector = selector.replace(/([^\s:~+>])\./g, '$1 &.')
@@ -11,6 +11,27 @@ exports.selectorPath = (selector, media) => {
 	return path
 }
 
-exports.createStyleBlocks = (root, media) => {
+const createStyleBlocks = (parent, media) => {
+	let blocks = []
+	parent.nodes.forEach(node => {
+		switch(node.type) {
+			case "rule":
+				blocks.push({
+					path: selectorPath(node.selector, media),
+					nodes: node.nodes,
+				})
+				break;
+			case "atrule":
+				let atruleBlocks = createStyleBlocks(node, `@${node.name} ${node.params}`)
+				blocks = blocks.concat(atruleBlocks)
+				break;
+		}
+	})
 
+	return blocks
+}
+
+module.exports = {
+	selectorPath,
+	createStyleBlocks,
 }
